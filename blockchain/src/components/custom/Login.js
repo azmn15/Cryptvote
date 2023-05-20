6eeef7c5203032d417ef1f1b6fb9611f4831721b
azmn <59349537+azmn15@users.noreply.github.com>
2023-05-21 00:39:35 +0800

    first commit

---
+++ blockchain/src/components/custom/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost:8000/api/adminLogin', {
+      username: username,
+      password: password,
+    })
+    .then(function(response){ 
+      if(response.data){
+        window.location.assign("/newelection")
+      }else{
+        alert('Incorrect Username or Password');
+      }
+    })
+    .catch(function(err){
+      console.error(err);
+    });
+  };
+
+  return (
+    <form className='container' onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
