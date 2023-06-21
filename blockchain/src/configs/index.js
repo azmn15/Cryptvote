27b0868c5366483ded5de184abbce0a4664e861c
azmn <59349537+azmn15@users.noreply.github.com>
2023-06-21 15:46:17 +0800

    configserver

--- blockchain/src/configs/index.js
+++ blockchain/src/configs/index.js
@@ -1,3 +1,3 @@
-// const configserver = "http://58.71.195.28:6980";
-const configserver = "http://localhost:8000";
+const configserver = "http://121.122.87.32:6980";
+//const configserver = "http://localhost:8000";
 export default configserver;
